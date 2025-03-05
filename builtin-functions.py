028fe77cf40d29b78ae9a17987ed70f63f5315c7
Zharkyn00 <zharkynkenzhebay@gmail.com>
2025-03-05 14:29:26 +0500

    Update builtin-functions.py

--- builtin-functions.py
+++ builtin-functions.py
@@ -14,7 +14,11 @@ def task2(s):
     lower = sum(1 for char in s if char.islower())
     return upper, lower
 
-print(task2("HelloWorld"))  # (2, 8)
+text = "Hello World!"
+upper, lower = count_letters(text)
+
+print("Upper case letters:", upper)
+print("Lower case letters:", lower)
 
 # 3
 def task3(s):
@@ -24,15 +28,27 @@ print(task3("radar"))  # True
 print(task3("hello"))  # False
 
 # 4
-def task4(number, delay_ms):
-    time.sleep(delay_ms / 1000)
-    return math.sqrt(number)
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")  
+    return s == "".join(reversed(s)) 
 
-print(task4(25100, 2123))  # 158.42979517754858 (және кідіріс 2.123 секунд)
+word1 = "madam"
+word2 = "hello"
+
+print(f'Is "{word1}" a palindrome?', is_palindrome(word1))
+print(f'Is "{word2}" a palindrome?', is_palindrome(word2))
+
 
 # 5
-def task5(tpl):
-    return all(tpl)
+def all_true(t):
+    r = True
+    for v in t:
+        r = r and v  
+    return r
 
-print(task5((True, True, False)))  # False
-print(task5((1, "hello", 3.5)))  # True
+t1 = (True, True, True)
+t2 = (True, False, True)
+
+print(all_true(t1))  # True
+print(all_true(t2))  # False
+
